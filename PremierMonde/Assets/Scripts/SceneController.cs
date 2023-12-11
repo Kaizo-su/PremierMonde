49745f3470caf19bdadc4f7c40a0380214d5ad78
gautiersliwa <gautier.sliwa@gmail.com>
2023-12-11 01:56:05 +0100

    Scene Transition part 1
    Add scene transition script

---
+++ PremierMonde/Assets/Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*public class SceneController : MonoBehaviour
+{
+	public string scenename;
+		
+	void OnMouseDown()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			SceneManager.LoadScene(scenename);
+		}
+	}
+}
+*/
+
+public class SceneController : MonoBehaviour
+{
+	
+	public string scenename;
+ 
+ void OnTriggerEnter(Collider other)
+ 
+ {
+ 	
+	 if(other.CompareTag("Player"))
+	 {
+		SceneManager.LoadScene(scenename);
+	 }
+ }
+ 
+}
