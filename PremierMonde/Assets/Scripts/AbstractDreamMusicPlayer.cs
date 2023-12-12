fc9cb18024717527b952443bdf601f43b062f95d
Kaizo-su <kaizo-su@hotmail.com>
2023-12-11 23:51:13 -0500

    Abstract dream Order

--- PremierMonde/Assets/Scripts/AbstractDreamMusicPlayer.cs
+++ PremierMonde/Assets/Scripts/AbstractDreamMusicPlayer.cs
@@ -11,23 +11,27 @@ public class AbstractDreamMusicPlayer : MonoBehaviour
     public int melodyDuration;
     public int validationRange;
 	public int silenceBetweenLoops;
-	//public string melody;
+    public int id;
+    //public string melody;
 
     [Range(0, 1)]
     public float[] Partition;
 
     private int timer;
-    private int id;
     private int index = 0;
     private int validation = 0;
 	private bool isPlaying = false;
 
 
 	private ParticleSystem EchoWaveGenerator;
+	private ParticleSystem EchoValidationGenerator;
+	private ParticleSystem EchoCompletedGenerator;
+	private ParticleSystem EchoErrorGenerator;
 	private StudioEventEmitter fmodMelody;
 
+    private static int progression = 0;
     private static GameObject MusicMaster;
-    private static bool[] IDs;
+    private static Transform Player;
 
     // Start is called before the first frame update
     void Start()
@@ -37,85 +41,96 @@ public class AbstractDreamMusicPlayer : MonoBehaviour
             MusicMaster = GameObject.Find("AbstractDreamMusicMaster");
             MusicMaster.SetActive(false);
         }
-
-        id = (IDs == null ? 0 : IDs.Length);
-	    Array.Resize(ref IDs, id + 1);
         
 	    fmodMelody = this.GetComponent<StudioEventEmitter>();
 
         timer = - validationRange;
         silenceBetweenLoops -= validationRange;
+
+        if(Player == null)
+        Player = GameObject.Find("PlayerVR").transform;
+
         EchoWaveGenerator = this.transform.GetChild(0).GetComponent<ParticleSystem>();
+        EchoValidationGenerator = this.transform.GetChild(1).GetComponent<ParticleSystem>();
+        EchoCompletedGenerator = this.transform.GetChild(2).GetComponent<ParticleSystem>();
+        EchoErrorGenerator = this.transform.GetChild(3).GetComponent<ParticleSystem>();
     }
 
     // Update is called once per fixed frame
     void FixedUpdate()
     {
-        // Manage time
-        if (timer >= melodyDuration + silenceBetweenLoops)
+        Debug.Log(Vector3.Distance(this.transform.position, Player.position));
+
+        if (Vector3.Distance(this.transform.position, Player.position) < 12)
+        {
+            // Manage time
+            if (timer >= melodyDuration + silenceBetweenLoops)
+            {
+                timer = -validationRange;
+                index = 0;
+                validation = 0;
+            }
+
+            if (index < Partition.Length && timer == (int)(Partition[index] * melodyDuration) - validationRange)
+            {
+                EchoWaveGenerator.Play();
+            }
+
+            // Manage Melody
+
+            if (timer == 0)
+            {
+                fmodMelody.Play();
+            }
+
+            if (index < Partition.Length && timer == (int)(Partition[index] * melodyDuration))
+            {
+                isPlaying = true;
+                index++;
+            }
+            else
+            {
+                isPlaying = false;
+            }
+
+            // Manage Clock
+            timer++;
+        }
+        else
         {
             timer = - validationRange;
             index = 0;
             validation = 0;
         }
-
-        if (index < Partition.Length && timer == (int) (Partition[index] * melodyDuration) - validationRange)
-        {
-            EchoWaveGenerator.Play();
-        }
-
-	    // Manage Melody
-	    
-	    if (timer == 0 ){
-	    	//Debug.Log ("quoicoubeh");
-	    	//FMODUnity.RuntimeManager.PlayOneShot("event:/" + melody);
-	    	fmodMelody.Play();
-	    	
-	    }
         
-        if (index < Partition.Length && timer == (int) (Partition[index] * melodyDuration))
-        {
-            
-            isPlaying = true;
-            index++;
-        }    
-        else
-        {
-            isPlaying = false;
-        }
-
-        // Manage Clock
-        timer++;
 
     }
 
     private void OnTriggerStay(Collider other)
     {
-
+        // Collision avec la sphere qui defini la porté du son
         if (isPlaying)
         {
+            EchoValidationGenerator.Play();
             validation++;
 
-
-            // Victory validation
+            // Valide que la musique a été joué entierement.
             if (validation == Partition.Length)
             {
-
-                if (id == 0 || IDs[id - 1] == true)
+                // Valide que celui qui joue est le bon
+                if (id == progression)
                 {
-                    IDs[id] = true;
-                    this.GetComponent<Renderer>().material.color = Color.green;
+                    //this.GetComponent<Renderer>().material.color = Color.green;
+                    EchoCompletedGenerator.Play();
+                    progression++;
 
-                    foreach(bool i in IDs)
-                    {
-                        Debug.Log(i);
-
-                        if (!i)
-                        {
-                            return;
-                        }
-                    }
-                    MusicMaster.SetActive(true);
+                    if (progression >= 4)
+                        MusicMaster.SetActive(true);
+                }
+                else
+                {
+                    progression = 0;
+                    EchoErrorGenerator.Play();
                 }
             }
         }
